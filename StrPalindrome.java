79f5028823834e9c5d1fe4b0491be498a44f83e5
Rampally Narsimha <147978480+23315a1204Narsimha@users.noreply.github.com>
2023-10-28 10:51:57 +0530

    Update StrPalindrome.java

--- StrPalindrome.java
+++ StrPalindrome.java
@@ -5,7 +5,7 @@ class StrPalindrome
    {  
       String original, reverse=""; // Objects of String class  
       Scanner in = new Scanner(System.in);   
-      System.out.println("Enter a string/number to check if it is a palindrome");  
+      System.out.println("Enter a string");  
       original = in.nextLine();   
       int length = original.length();   
       for ( int i = length - 1; i >= 0; i-- )  
