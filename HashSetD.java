211f6ddfef598db3a22d1ad6ee85e60ccfebfe30
NarsimhasreenidhiIT-A <147978480+NarsimhasreenidhiIT-A@users.noreply.github.com>
2023-11-10 17:10:19 +0530

    origin/main

---
+++ HashSetD.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class HashSetD
+{
+    public static void main(String[] args) {
+        HashSet hs=new HashSet();
+        hs.add("B");
+        hs.add("F");
+         hs.add("V");
+          hs.add("A");
+           hs.add("Z");
+           System.out.println(hs);
+    }
+}
