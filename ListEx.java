77b5ac0aeef78c6cbd47f4de479867f1ac06b02c
NarsimhasreenidhiIT-A <147978480+NarsimhasreenidhiIT-A@users.noreply.github.com>
2023-11-10 16:36:23 +0530

    origin/main

---
+++ ListEx.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class ListEx
+{
+    public static void main(String args[])
+    {
+        List ls=new ArrayList<>();
+        ls.add("a");
+        ls.add("d");
+        ls.add("d");
+        System.out.println(ls);
+        System.out.println(ls.get(0));
+    }
+}
