d578198000bf0a470b88925bda1fe84b7a33c8ae
NarsimhasreenidhiIT-A <147978480+NarsimhasreenidhiIT-A@users.noreply.github.com>
2023-11-10 16:16:34 +0530

    origin/main

--- SortInterface.java
+++ SortInterface.java
@@ -1,27 +1,5 @@
-<<<<<<< HEAD
-import java.util.*;
-class ArrayListD
-{
-public static void main(String args[])
-{
-ArrayList al=new ArrayList();
-al.add("6");
-al.add("3");
-al.add("0");
-al.add("4");
-al.add("5");
-al.add("1");
-al.add("3");
-al.add("7");
-al.add("0");
-al.add("2");
-System.out.println(al);
-}
-}
-=======
 package sortapp;
 public interface SortInterface
 {
 public void sort();
 }
->>>>>>> d8a3b8392c239786f00d097634b4fdbb6f95f660
