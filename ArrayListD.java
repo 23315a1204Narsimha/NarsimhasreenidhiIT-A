14310b5323f37fcf11a9c12f61a1140bef702c13
NarsimhasreenidhiIT-A <147978480+NarsimhasreenidhiIT-A@users.noreply.github.com>
2023-11-10 16:19:41 +0530

    origin/main

---
+++ ArrayListD.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class ArrayListD
+{
+public static void main(String args[])
+{
+ArrayList al=new ArrayList();
+al.add("6");
+al.add("3");
+al.add("0");
+al.add("4");
+al.add("5");
+al.add("1");
+al.add("3");
+al.add("7");
+al.add("0");
+al.add("2");
+System.out.println(al);
+}
+}
