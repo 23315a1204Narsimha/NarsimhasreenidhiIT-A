284d7ce93b03f1d44b8baf5cf8408f0a08d9bc7a
Rampally Narsimha <147978480+23315a1204Narsimha@users.noreply.github.com>
2023-12-27 21:00:41 +0530

    Add files via upload

--- ButtonDemo.java
+++ ButtonDemo.java
@@ -1,18 +1,25 @@
 import java.awt.*;
-
-public class ButtonDemo extends Frame
+import java.awt.event.*;
+class MyActionListener implements ActionListener
 {
+   public void actionPerformed(ActionEvent ae)
+    {
+        System.out.println("Button pressed");
+    }
+}
+class ButtonDemo extends Frame{
     ButtonDemo()
     {
-        super("Button Demo");
+        MyActionListener al=new MyActionListener();
         setVisible(true);
         setSize(500,500);
-        Button b1=new Button("Click here");
-        b1.setSize(10,20);
+        Button b1=new Button("Click Here");
+        b1.setSize(10,10);
+        b1.addActionListener(al);
         add(b1);
     }
     public static void main(String args[])
     {
-        new ButtonDemo();
+        ButtonDemo bd=new ButtonDemo();
     }
 }
